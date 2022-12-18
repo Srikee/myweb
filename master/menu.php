caaee4830bef81fbad5ca6657547790d4a12f020
Srikee <kee19384@gmail.com>
2022-12-18 15:45:19 +0700

    sass

--- master/menu.php
+++ master/menu.php
@@ -1,13 +1,25 @@
 <div class="list-group">
-    <a href="./" class="list-group-item list-group-item-action <?php if($page=="home") echo "active"; ?>">หน้าแรก</a>
+    <a href="./" class="list-group-item list-group-item-action <?php if($page=="home") echo "active"; ?>">
+        <i class="fa-solid fa-house"></i> หน้าแรก
+    </a>
     <a href="./?page=deposit"
-        class="list-group-item list-group-item-action <?php if($page=="deposit") echo "active"; ?>">ฝากเงิน</a>
+        class="list-group-item list-group-item-action <?php if($page=="deposit") echo "active"; ?>">
+        <i class="fa-solid fa-hand-holding-dollar"></i> ฝากเงิน
+    </a>
     <a href="./?page=widthdraw"
-        class="list-group-item list-group-item-action <?php if($page=="widthdraw") echo "active"; ?>">ถอนเงิน</a>
+        class="list-group-item list-group-item-action <?php if($page=="widthdraw") echo "active"; ?>">
+        <i class="fa-solid fa-dollar-sign"></i> ถอนเงิน
+    </a>
     <a href="./?page=check-balance"
-        class="list-group-item list-group-item-action <?php if($page=="check-balance") echo "active"; ?>">ตรวจสอบยอดเงิน</a>
+        class="list-group-item list-group-item-action <?php if($page=="check-balance") echo "active"; ?>">
+        <i class="fa-solid fa-spell-check"></i> ตรวจสอบยอดเงิน
+    </a>
     <a href="./?page=account"
-        class="list-group-item list-group-item-action <?php if($page=="account" || $page=="account-add" || $page=="account-edit") echo "active"; ?>">ข้อมูลสมุดบัญชี</a>
+        class="list-group-item list-group-item-action <?php if($page=="account" || $page=="account-add" || $page=="account-edit") echo "active"; ?>">
+        <i class="fa-solid fa-book"></i> ข้อมูลสมุดบัญชี
+    </a>
     <a href="./logout.php" onclick="return confirm('คุณแน่ใจต้องการออกจากระบบใช่หรือไม่ ?')"
-        class="list-group-item list-group-item-action">ออกจากระบบ</a>
+        class="list-group-item list-group-item-action">
+        <i class="fa-solid fa-right-from-bracket"></i> ออกจากระบบ
+    </a>
 </div>
